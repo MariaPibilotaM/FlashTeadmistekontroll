5b752af1b363eba6975c8c656ab57fa7968a596f
Maria <mariamurumaaa@gmail.com>
2019-05-14 15:17:36 +0300

    muutus

--- mustruut-gradle/src/main/java/oop/küsimused.java
+++ mustruut-gradle/src/main/java/oop/küsimused.java
@@ -1,5 +1,84 @@
 package oop;
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
-public class küsimused {
+
+public class küsimused extends Application {
+    String küsimus;
+    String vastus;
+
+    public küsimused(String Küsimus,String Vastus){
+        küsimus = Küsimus;
+        vastus = Vastus;
+    }
+    @Override
+
+    public void start(Stage primaryStage) {
+        String[] vastusevariandid = vastus.split(" "); //splitime lisatavad
+        //Pealava nimetuse määramine
+        primaryStage.setTitle("Testike");
+        //Küsimuse lisamine
+        Label labelfirst= new Label(küsimus);
+        Label labelresponse= new Label();
+
+        //Vastamise nupp
+        Button button= new Button("Vasta");
+        //Valikvõimaluste lisamine
+        RadioButton radio1, radio2, radio3;
+        radio1=new RadioButton(vastusevariandid[0]);
+        radio2= new RadioButton(vastusevariandid[1]);
+        radio3= new RadioButton(vastusevariandid[3]);
+
+        //Grupi loomine
+        ToggleGroup question1= new ToggleGroup();
+
+        radio1.setToggleGroup(question1);
+        radio2.setToggleGroup(question1);
+        radio3.setToggleGroup(question1);
+        //Vaikesätted
+        button.setDisable(true);
+
+        radio1.setOnAction(e -> button.setDisable(false) );
+        radio2.setOnAction(e -> button.setDisable(false) );
+        radio3.setOnAction(e -> button.setDisable(false) );
+        //Väärtustamine
+        button.setOnAction(e ->
+                {
+
+                    if (radio3.isSelected())
+                    {
+                        labelresponse.setText("Õige vastus");
+                        button.setDisable(true);
+                    }
+
+                    else
+                    {
+                        labelresponse.setText("Vale vastus");
+                        button.setDisable(true);
+                    }
+                }
+        );
+
+        VBox layout= new VBox(5);
+
+        layout.getChildren().addAll(labelfirst, radio1, radio2, radio3, button, labelresponse);
+
+        Scene scene1= new Scene(layout, 400, 250);
+        primaryStage.setScene(scene1);
+
+        primaryStage.show();
+    }
+
+
+    /*public static void main(String[] args) {
+        launch(args);
+    }*/
 
 }
--- mustruut-gradle/src/main/java/oop/mäng.java
+++ mustruut-gradle/src/main/java/oop/mäng.java
@@ -1,10 +1,19 @@
 package oop;
 
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+import static javafx.application.Application.launch;
+
 public class mäng {
     public static void main(String[] args) {
         String k1 = "Mis värvi on banaan?";
         String vastused = "kollane punane lilla";
+        küsimused uus = new küsimused(k1,vastused);
+        uus.start();
+        launch(args);
         String k2 = "Mis värvi on maasikas?";
         String vastused2="kollane punane lilla";
+        küsimused uus2 = new küsimused(k2,vastused2);
     }
 }
