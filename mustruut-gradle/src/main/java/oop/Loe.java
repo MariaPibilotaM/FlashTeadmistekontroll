0d3bf8a18521df362cafa593475b6c3b56769d35
Maria <mariamurumaaa@gmail.com>
2019-05-15 19:10:14 +0300

    ee, vb

--- mustruut-gradle/src/main/java/oop/Loe.java
+++ mustruut-gradle/src/main/java/oop/Loe.java
@@ -4,6 +4,7 @@ import java.io.*;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 
 public class Loe {
     public String loe() throws IOException {
@@ -11,13 +12,12 @@ public class Loe {
         String read = Files.readString(fail.toPath(), Charset.forName("UTF-8"));
         return read;
     }
-    public void kirjuta(String sõna) throws IOException {
+    public void kirjuta(ArrayList sõna) throws IOException {
         BufferedWriter writer = new BufferedWriter(new FileWriter("tulemus.txt"));
-        /*for (int i = 0; i < sõna.length; i++) {
+        for (int i = 0; i < sõna.length; i++) {
             writer.write(sõna[i]+"\n");
-        }*/
-        writer.append(sõna);
-        writer.append("\n");
-        //writer.close();
+
+
+        writer.close();
     }
 }
